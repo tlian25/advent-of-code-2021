b953b18ffe85556f796257ca754e479a89acd6bb
txlian <txlian@amazon.com>
2023-04-10 11:59:14 -0400

    day19 incomplete

---
+++ solutions/day19_beacon_scanner.py
@@ -0,0 +1,26 @@
+# Day 
+# https://adventofcode.com/2021/day/
+
+from util.input_util import read_input_file
+
+def parse_lines():
+    lines = read_input_file(0)
+
+    
+# 1000 units away from scanner
+# Find pairs of scanners that both see at least 12 of the same beacons
+
+def solution1():
+    pass
+    
+def solution2():
+    pass    
+    
+    
+    
+if __name__ == '__main__':
+    print(solution1())
+    
+    print('--------------')
+    
+    print(solution2())
